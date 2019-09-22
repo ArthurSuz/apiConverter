fc43778ed811247a898bfe4fa5f668ae417d22a1
苏镇宇 <suzhenyu@qutoutiao.net>
2019-09-22 10:48:19 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+        <el-container>
+            <el-header class="header">API-Converter</el-header>
+            <el-main>
+                <Home />
+            </el-main>
+        </el-container>
+    </div>
+</template>
+
+<script>
+import Home from "./views/Home.vue";
+
+export default {
+    name: "app",
+    components: {
+        Home
+    }
+};
+</script>
+
+<style lang="less" scoped>
+#app {
+    .header {
+        display: flex;
+        align-items: center;
+        background-color: #fff;
+        width: 100%;
+        border-bottom: 1px solid #dcdfe6;
+        font-size: 32px;
+    }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,180 @@
+<template>
+    <div id="home">
+        <el-card class="pane back">
+            <div class="backLists" v-for="(backList,index) in backLists" :key="index">
+                <el-input v-model="backList.name" @change="change">
+                    <template slot="prepend">结构名</template>
+                </el-input>
+                <div class="value" v-for="(item,index) in backList.data" :key="index">
+                    <el-input v-model="item.input" @change="change" placeholder="字段"></el-input>
+                    <el-select v-model="item.type" @change="change" placeholder="数据类型">
+                        <el-option v-for="option in options" :key="option" :value="option"></el-option>
+                    </el-select>
+                </div>
+                <el-button class="add-value" @click="addValue(index)">添加字段+</el-button>
+            </div>
+            <el-button class="add-value" @click="addList">添加结构+</el-button>
+        </el-card>
+        <el-card class="pane front">
+            <el-button @click="changeJson" size="small">{{showDoubleQuotes?`切换Vue Data`:`切换标准JOSN`}}</el-button>
+            <vue-json-pretty :showDoubleQuotes="showDoubleQuotes" :data="jsonText"></vue-json-pretty>
+        </el-card>
+    </div>
+</template>
+
+<script>
+import VueJsonPretty from "vue-json-pretty";
+const basisTypes = {
+    Int: `0`,
+    Date: `"2019-09-21"`,
+    String: `"test"`,
+    "Arr<int>": `[1, 2, 3, 4, 5]`,
+    "Arr<string>": `["a", "b", "c", "d", "e"]`,
+    timestamp: `1569054307819`
+};
+const complexTypes = {
+    "List<Obj>": `[{}]`,
+    Obj: `{}`
+};
+const responeType = "Obj";
+const template = {
+    name: "respone",
+    data: [
+        {
+            input: "code",
+            type: "Int"
+        },
+        {
+            input: "message",
+            type: "String"
+        },
+        {
+            input: "data",
+            type: "Obj"
+        }
+    ]
+};
+export default {
+    name: "home",
+    components: {
+        VueJsonPretty
+    },
+    data() {
+        return {
+            options: [],
+            backListItem: {
+                input: "",
+                type: ""
+            },
+            backListsItem: {
+                name: "data",
+                data: [
+                    {
+                        input: "",
+                        type: ""
+                    }
+                ]
+            },
+            types: { ...basisTypes, ...complexTypes },
+            backLists: [],
+            jsonText: "",
+            showDoubleQuotes: true
+        };
+    },
+    mounted() {
+        this.options = [
+            ...Object.keys(basisTypes),
+            ...Object.keys(complexTypes)
+        ];
+        this.backLists.push(template);
+        this.change();
+    },
+    methods: {
+        change() {
+            if (this.backLists[0]) {
+                this.jsonText = JSON.parse(
+                    this.transform(this.backLists[0].data, responeType)
+                );
+            }
+        },
+        addValue(index) {
+            this.backLists[index].data.push(this.newOBJ(this.backListItem));
+        },
+        addList() {
+            this.backLists.push(this.newOBJ(this.backListsItem));
+        },
+        newOBJ(obj) {
+            return JSON.parse(JSON.stringify(obj));
+        },
+        transform(data, type, index = 0) {
+            if (index > 3) {
+                this.$message.error(`嵌套最多${index}层, 请勿循环嵌套`);
+                return;
+            }
+            if (!data) {
+                return;
+            }
+            let str = "";
+            data.forEach(item => {
+                if (item.input) {
+                    if (Object.keys(basisTypes).includes(item.type)) {
+                        str += `"${item.input}":${this.types[item.type]},`;
+                    } else if (Object.keys(complexTypes).includes(item.type)) {
+                        index++;
+                        let childStr = this.transform(
+                            this.findInList(item.input, index),
+                            item.type,
+                            index
+                        );
+                        str += `"${item.input}":${childStr ||
+                            this.types[item.type]},`;
+                    }
+                }
+            });
+            switch (type) {
+                case "Obj":
+                    str = str ? `{${str}}` : "";
+                    break;
+                case "List<Obj>":
+                    str = str ? `[{${str}},{${str}},{${str}}]` : "";
+                    break;
+            }
+            return str.replace(/,}/g, "}");
+        },
+        findInList(name, index) {
+            let res = this.backLists.slice(index).find(item => {
+                return item.name === name;
+            });
+            return res && res.data;
+        },
+        changeJson() {
+            this.showDoubleQuotes = !this.showDoubleQuotes;
+        }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+#home {
+    display: flex;
+    .pane {
+        flex: 1;
+        margin: 0 10px;
+    }
+    .back {
+        display: flex;
+        flex-direction: column;
+        .value {
+            display: flex;
+        }
+        .add-value {
+            width: 100%;
+        }
+        .backLists {
+            margin-bottom: 20px;
+            border: 1px #909399 solid;
+            border-radius: 5px;
+        }
+    }
+}
+</style>
